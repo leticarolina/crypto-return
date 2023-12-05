f645f2f58d12ef8aea869e75811f32b5c064f15d
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2023-12-05 09:42:01 -0300

    fetch

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,46 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 function App() {
-  return "hi";
+  const [coinData, setCoinData] = useState(null);
+  const [coinInput, setCoinInput] = useState("");
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("https://api.minerstat.com/v2/coins?list=${coinInput}")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setCoinData(data);
+        })
+        .catch((error) => {
+          console.error("There was a problem fetching the data:", error);
+        });
+    };
+
+    fetchData();
+  });
+
+  return (
+    <div>
+      {coinData ? <div>{JSON.stringify(coinData)}</div> : <div>Loading...</div>}
+      <form
+        action="
+      "
+      >
+        <label htmlFor="coinInput"> Coin:</label>
+        <input
+          type="text"
+          id="coinInput"
+          value={coinInput}
+          onChange={(e) => setCoinInput(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default App;
