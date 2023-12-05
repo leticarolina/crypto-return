b1738fb7868174905d488c207a127abab31d0cc0
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2023-12-05 16:19:47 -0300

    fetch

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,41 +4,46 @@ import { useState } from "react";
 function App() {
   const [coinData, setCoinData] = useState(null);
   const [coinInput, setCoinInput] = useState("");
-  useEffect(() => {
-    const fetchData = () => {
-      fetch("https://api.minerstat.com/v2/coins?list=${coinInput}")
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setCoinData(data);
-        })
-        .catch((error) => {
-          console.error("There was a problem fetching the data:", error);
-        });
-    };
+  const [loading, setLoading] = useState(false);
 
-    fetchData();
-  });
+  function fetchData() {
+    setLoading(true);
+    fetch(`https://api.minerstat.com/v2/coins?list=${coinInput}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCoinData(data);
+      })
+      .catch((error) => {
+        console.error("There was a problem fetching the data:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
 
   return (
     <div>
-      {coinData ? <div>{JSON.stringify(coinData)}</div> : <div>Loading...</div>}
-      <form
-        action="
-      "
-      >
-        <label htmlFor="coinInput"> Coin:</label>
-        <input
-          type="text"
-          id="coinInput"
-          value={coinInput}
-          onChange={(e) => setCoinInput(e.target.value)}
-        />
-      </form>
+      {/* {coinData ? (
+        <div>{JSON.stringify(coinData[0].price)}</div>
+      ) : (
+        <div>Loading...</div>
+      )} */}
+      {coinData && <div>{JSON.stringify(coinData[0].price)}</div>}
+      {loading && <div>Loading!</div>}
+
+      <label htmlFor="coinInput"> Coin:</label>
+      <input
+        type="text"
+        id="coinInput"
+        value={coinInput}
+        onChange={(e) => setCoinInput(e.target.value)}
+      />
+      <button onClick={fetchData}>Submit</button>
     </div>
   );
 }
